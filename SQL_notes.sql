8b1e8c3f77b05f609e9a1ea7c5d53830ceb91fd4
Tyler Moazed <tylermoazed@gmail.com>
2017-10-27 17:38:34 +0200

    Some JOIN practice

--- SQL_notes.sql
+++ SQL_notes.sql
@@ -212,3 +212,87 @@ FROM accounts
 WHERE (name LIKE 'C%' OR name LIKE 'W%') AND 
 ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') AND primary_poc NOT LIKE '%eana%');
 
+JOINS
+
+/*
+Essentially ideas that are aimed at database normalization:
+
+Are the tables storing logical groupings of the data?
+Can I make changes in a single location, rather than in many tables for the same information?
+Can I access and manipulate data quickly and efficiently?
+*/
+
+SELECT orders.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+/*
+Try pulling all the data from the accounts table, and all the data from the orders table.
+*/
+SELECT orders.*, accounts.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+
+/*
+Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the 
+website and the primary_poc from the accounts table.
+*/
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website,
+accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+/*
+ALIAS examples
+
+FROM tablename t1
+JOIN tablename2 t2
+
+and
+
+SELECT col1 + col2 total, col3
+
+- a space after the actual table or column name creates an alias
+*/
+
+/*
+Provide a table for all web_events associated with account name of Walmart. 
+There should be three columns. Be sure to include the primary_poc, time of the event, 
+and the channel for each event. Additionally, you might choose to add a fourth column 
+to assure only Walmart events were chosen. 
+*/
+
+SELECT a.name, a.primary_poc, w.occurred_at, w.channel
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+/*
+Provide a table that provides the region for each sales_rep along with their associated 
+accounts. Your final table should include three columns: the region name, the sales rep 
+name, and the account name. Sort the accounts alphabetically (A-Z) according to account 
+name. 
+*/
+
+SELECT r.name region, s.name rep, a.name account
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY a.name;
+
+
+/*
+Provide the name for each region for every order, as well as the account name and 
+the unit price they paid (total_amt_usd/total) for the order. Your final table should 
+have 3 columns: region name, account name, and unit price. A few accounts have 0 for 
+total, so I divided by (total + 0.01) to assure not dividing by zero.
+*/
+
