1bbf05e03c42323b7839b428d6523cb33827901b
Tyler Moazed <tylermoazed@gmail.com>
2017-10-27 11:16:17 +0200

    Finished Basic SQL commands

--- SQL_notes.sql
+++ SQL_notes.sql
@@ -70,3 +70,145 @@ SELECT *
 FROM orders
 WHERE total_amt_usd < 500
 LIMIT 10;
+
+/*
+Filter the accounts table to include the company name, website, and the primary point 
+of contact for Exxon Mobil in the accounts table.
+*/
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+/*Arithmetic Operators:
+Create a column that divides the standard_amt_usd by the standard_qty to find the unit 
+price for standard paper for each order. Limit the results to the first 10 orders, and 
+include the id and account_id fields
+*/
+SELECT id, account_id, standard_amt_usd / standard_qty AS unit_price_usd
+FROM orders
+LIMIT 10;
+
+/*
+Write a query that finds the percentage of revenue that comes from poster paper for each 
+order. You will need to use only the columns that end with _usd. (Try to do this without 
+using the total column). Include the id and account_id fields.
+*/
+
+SELECT id, account_id, 
+poster_amt_usd/(standard_amt_usd+gloss_amt_usd+poster_amt_usd) AS poster_pct
+FROM orders
+LIMIT 10;
+
+/*
+avoid divide by zero error */
+
+SELECT id, account_id, 
+poster_amt_usd/(standard_amt_usd+gloss_amt_usd+poster_amt_usd+.0001) AS poster_pct
+FROM orders;
+
+/*
+Use the accounts table to find
+
+All the companies whose names start with 'C'. 
+
+All companies whose names contain the string 'one' somewhere in the name.
+
+All companies whose names end with 's'.
+*/
+
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%' OR name LIKE '%one%' OR name like '%s';
+
+/*
+Questions using IN operator
+
+Use the accounts table to find the account name, primary poc, and sales rep id for 
+Walmart, Target, and Nordstrom.
+*/
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+/*
+Use the web_events table to find all information regarding individuals who were 
+contacted via organic or adwords.
+*/
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
+
+/*
+Questions using the NOT operator
+We can pull all of the rows that were excluded from the queries in the previous 
+two concepts with our new operator.
+*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords');
+
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name NOT LIKE '%one%' AND name NOT like '%s';
+
+/*
+Write a query that returns all the orders where the standard_qty is over 1000, 
+the poster_qty is 0, and the gloss_qty is 0.
+*/
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+/*
+Using the accounts table find all the companies whose names 
+do not start with 'C' and end with 's'.
+*/
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name NOT like '%s';
+
+
+/*
+Use the web_events table to find all information regarding individuals who were 
+contacted via organic or adwords and started their account at any point in 2016 sorted 
+from newest to oldest.
+*/
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+/*
+Questions using the OR operator
+Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. 
+Only include the id field in the resulting table.
+*/
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
+/*
+Write a query that returns a list of orders where the standard_qty is zero and either 
+the gloss_qty or poster_qty is over 1000.
+*/
+
+SELECT *
+FROM orders
+WHERE (gloss_qty > 1000 OR poster_qty > 1000) AND standard_qty = 0;
+
+/*
+Find all the company names that start with a 'C' or 'W', and the primary contact contains 
+'ana' or 'Ana', but it doesn't contain 'eana'.
+*/
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%') AND 
+((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') AND primary_poc NOT LIKE '%eana%');
+
